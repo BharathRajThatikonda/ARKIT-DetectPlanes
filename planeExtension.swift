8c6f59198055cf69410617acd7e6077a87bf934b
Bharath Thatikonda <bthatikonda@gemini-us.com>
2018-06-27 19:17:36 +0530

    ARKIT- to detect planes

---
+++ planeExtension.swift
@@ -0,0 +1,17 @@
+//
+//  planeExtension.swift
+//  detectingPlanesSamleProject
+//
+//  Created by Guest1 on 16/10/17.
+//  Copyright © 2017 GCS. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+extension ARPlaneAnchor {
+    func addPlaneNode(on node : SCNNode,color:UIColor)  {
+        print("adding\(node),color\(color)")
+    }
+    
+}
